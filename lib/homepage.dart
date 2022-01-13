fc77e808d5196f84f2c831bcdbf1d91069b3daa9
maanu664 <maanu664@gmail.com>
2022-01-13 20:21:28 +0500

    update

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  final int day = 30;
+  final String name = "coding";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coding Strom Catalog"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("this $day course of $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
