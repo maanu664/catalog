bb161ad2bbcfa1e403f5f556d2ee5ba81644b1fe
maanu664 <maanu664@gmail.com>
2022-01-17 18:36:02 +0500

    update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:catalog/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LogIn extends StatelessWidget {
@@ -5,55 +6,61 @@ class LogIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "images/login1.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "welcome",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "images/login1.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Name",
-                    labelText: "User Name",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  child: Text("Log In"),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    shadowColor: Colors.yellowAccent,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
+            SizedBox(
+              height: 10,
             ),
-          )
-        ],
+            Text(
+              "welcome",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Your Name",
+                      labelText: "User Name",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Your Password",
+                      labelText: "Password",
+
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    child: Text("Log In"),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shadowColor: Colors.yellowAccent,
+                      minimumSize: Size(120,50),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoutes);
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
